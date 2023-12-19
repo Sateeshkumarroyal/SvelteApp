cd436d4c8a8110ff0b9302e4cdc7bec250785c51
sateeshkumar <royalsateeshkumar@gmail.com>
2023-12-19 13:53:02 +0530

    Navigating Programtically

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,21 @@
+<script>
+import {goto, beforeNavigate, afterNavigate} from '$app/navigation';
+const handleclick = () => {
+    goto('/products);
+};
+
+beforeNavigate((navigate) => {
+    console.log({ before:navigation});
+});
+
+afterNavigateNavigate((navigate) => {
+    console.log({ after:navigation});
+});
+</script>
 <h1>Welcome Home</h1>
 <a href="/blog">Blog</a>
 <a href="/products">Products</a>
 <a href="/about">About</a>
 <a href="/profile">Profile</a>
+
+<button on:click={handleclick}>Submitt</button>
